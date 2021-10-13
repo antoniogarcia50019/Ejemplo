7d10ff8b11ab761e6ef587af58f5adfee6c5b302
AntonioGarcialll <62670455+AntonioGarcialll@users.noreply.github.com>
2021-10-13 18:30:04 -0500

    Agregué el como estas

--- src/ejemplogit2/EjemploGit2.java
+++ src/ejemplogit2/EjemploGit2.java
@@ -17,6 +17,8 @@ public class EjemploGit2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola");
+        System.out.println("como estas");
+        
     }
     
 }
